36e4ff437d5ae5b42dfc4478cb327a961ac092a6
Michael Pavelchuk <error2268@gmail.com>
2024-07-21 19:29:04 +0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import CambridgeLandingPage from "./cambridge/CambridgeLandingPage";
 import CambridgeStakeholderPages from "./cambridge/CambridgeStakeholderPages";
 import WisconsinLandingPage from "./wisconsin/WisconsinLandingPage";
@@ -17,57 +21,45 @@ import "./CustomerKeyFeatures.scss";
 import "./CustomerWhatWeDo.scss";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/profile/",
-      element: <MyProfile />,
-    },
-    {
-      path: "/profile/cam",
-      element: <CambridgeLandingPage />,
-    },
-    {
-      path: "/profile/cam/:activePage",
-      element: <CambridgeStakeholderPages />,
-    },
-    {
-      path: "/profile/uwmadison-cals",
-      element: <WisconsinLandingPage />,
-    },
-    {
-      path: "/profile/uwmadison-cals/:activePage",
-      element: <WisconsinStakeholderPages />,
-    },
-  ]);
-
   return (
     <>
-    <div className='my-profile'>
-      <nav className="navigation">
-        <div className="page-container">
-          {/* <div className="logo">
+      <div className="my-profile">
+        <nav className="navigation">
+          <div className="page-container">
+            {/* <div className="logo">
             <a href="#home"><img src="./public/logo.png" alt="Michael Pavelchuk logo" className="mp-logo"/></a>
           </div> */}
-          <ul className="navigation-list" id="navigation">
-            <li className="nav-item">
-              <a className="nav-link" href="/profile/">
-                About me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/profile/cam">
-                Cambridge
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/profile/uwmadison-cals">
-                Wisconsin
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav></div>
-      <RouterProvider router={router} />
+            <ul className="navigation-list" id="navigation">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  About me
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cam">
+                  Cambridge
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/uwmadison-cals">
+                  Wisconsin
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<MyProfile />} />
+        <Route path="/cam" element={<CambridgeLandingPage />} />
+        <Route path="/cam/:activePage" element={<CambridgeStakeholderPages />} />
+        <Route path="/uwmadison-cals" element={<WisconsinLandingPage />} />
+        <Route
+          path="/uwmadison-cals/:activePage"
+          element={<WisconsinStakeholderPages />}
+        />
+      </Routes>
     </>
   );
 }
